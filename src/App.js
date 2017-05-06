52068ba5dbc4199fe5af006aa0e2fffb868c5aca
Paul McConnell <paul.mcconnell@asynchrony.com>
2017-05-06 15:53:05 -0500

    simplify and de-emphasize header

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Five Star Feedback</h2>
+            <span className="App-tagline">Five Star Feedback</span>
+            <img src={logo} className="App-logo" alt="logo" />
         </div>
       </div>
     );
