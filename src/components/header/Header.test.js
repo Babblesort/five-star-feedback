659e5f57ce717780a64810d177f128859c472af2
Paul McConnell <paul.mcconnell@asynchrony.com>
2017-05-08 10:12:10 -0500

    test content of Header

--- src/components/header/Header.test.js
+++ src/components/header/Header.test.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
 
-it('renders without crashing', () => {
+it('renders', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Header />, div);
+});
+
+it('contains the header-tagline and logo', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Header />, div);
+  const tagline = div.querySelector('span.Header-tagline');
+  const logo = div.querySelector('img.Header-logo');
+  expect(tagline).not.toBe(null);
+  expect(logo).not.toBe(null);
 });
